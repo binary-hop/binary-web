165ab01aa1c902b95a9320532a9acfcdfc635565
Andre Castelo <mycatdoitbetter@MacBook-Air-de-Andre.local>
2021-11-19 00:07:40 -0300

    i18n and modal styles

--- components/Header/HeaderButton.tsx
+++ components/Header/HeaderButton.tsx
@@ -1,16 +1,31 @@
 import { Button } from '@chakra-ui/react';
 import { HeaderButtonProps } from './types';
 
-export const HeaderButton = ({ name, onClick, color = 'blue.100' }: HeaderButtonProps) => (
-  <Button
-    variant="link"
-    color={color}
-    fontFamily="barlow"
-    fontWeight="400"
-    _focus={{ outline: 'none' }}
-    fontSize="1.4rem"
-    onClick={onClick}
-  >
-    {name}
-  </Button>
-);
+export const HeaderButton = ({ name, onClick, color = 'blue.100' }: HeaderButtonProps) => {
+  const buttonProps = {
+    variant: 'ghost',
+    color,
+    fontFamily: 'barlow',
+    fontWeight: '400',
+    fontSize: '1.2rem',
+    onClick,
+    transition: 'all .3s ease-in-out',
+    _hover: { textDecoration: 'none', cursor: 'pointer', transform: 'scale(1.05)' },
+    size: 'sm',
+    px: '0.3rem',
+  };
+
+  if (name === 'Contact') {
+    return (
+      <Button _active={{ bgColor: 'transparent' }} {...buttonProps} bgColor="background" _focus={{ outline: 'none' }}>
+        {name}
+      </Button>
+    );
+  }
+
+  return (
+    <Button _active={{ bgColor: 'transparent' }} {...buttonProps}>
+      {name}
+    </Button>
+  );
+};
